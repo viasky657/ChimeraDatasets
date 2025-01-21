f3bd5673f972d05a37ef69c4f15498dde5aaaa6e
viasky657 <89562411+viasky657@users.noreply.github.com>
2025-01-21 15:28:48 -0600

    Update Safety_Dataset_loader.py

--- datasets/languages_and_audio/safety/Safety_Dataset_loader.py
+++ datasets/languages_and_audio/safety/Safety_Dataset_loader.py
@@ -2,3 +2,4 @@ from datasets import load_dataset
 
 ds = load_dataset("Unified-Language-Model-Alignment/Anthropic_HH_Golden")
 ds3 = load_dataset("nvidia/Aegis-AI-Content-Safety-Dataset-1.0")
+ds4 = load_dataset("nvidia/HelpSteer2")
